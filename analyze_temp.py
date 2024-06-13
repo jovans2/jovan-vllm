80dcf258a008cd1a791aa20d68d5ea8619fee9ad
Jovan Stojkovic <t-stojkovicj@microsoft.com>
2024-06-13 11:00:11 -0700

    msg

--- analyze_temp.py
+++ analyze_temp.py
@@ -1,8 +1,37 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-tempFile = open("dcgm_temp_monitor_sleep", "r")
+# tempFile = open("dcgm_temp_monitor_sleep", "r")
+# tempLines = tempFile.readlines()
+
+recoverFile = open("benchmark_temperature.txt", "r")
+recoverLines = recoverFile.readlines()
+recover_times = []
+for line in recoverLines:
+    if "All GPUs and memories are cold after" in line:
+        recover_times.append(float(line.split()[-1]))
+
+x_vals = []
+for ind in range(len(recover_times)):
+    x_vals.append(ind)
+plt.plot(x_vals, recover_times)
+plt.xlabel("Config")
+plt.ylabel("Recovery time [s]")
+plt.grid(axis="both")
+plt.show()
+plt.clf()
+
+tempFile = open("dcgm_monitor_temperature_a", "r")
 tempLines = tempFile.readlines()
+
+tempFile = open("dcgm_monitor_temperature_b", "r")
+tempLines = tempLines + tempFile.readlines()
+
+tempFile = open("dcgm_monitor_temperature_c", "r")
+tempLines = tempLines + tempFile.readlines()
+
+tempFile = open("dcgm_monitor_temperature_d", "r")
+tempLines = tempLines + tempFile.readlines()
 
 temperatures_mem = []
 for _ in range(8):
@@ -38,8 +67,10 @@ for line in tempLines:
         pass
 
 for gpuId in range(8):
-    temperatures_gpu[gpuId] = temperatures_gpu[gpuId]
-    temperatures_mem[gpuId] = temperatures_mem[gpuId]
+    temperatures_gpu[gpuId] = temperatures_gpu[gpuId][:32000]
+    temperatures_mem[gpuId] = temperatures_mem[gpuId][:32000]
+    powers_gpu[gpuId] = powers_gpu[gpuId][:32000]
+    frequencies_gpu[gpuId] = frequencies_gpu[gpuId][:32000]
 
 x_vals = []
 for ind, val in enumerate(temperatures_gpu[0]):
@@ -48,6 +79,7 @@ for gpuId in range(len(temperatures_gpu)):
     plt.plot(x_vals, temperatures_gpu[gpuId], label="GPU"+str(gpuId))
 plt.xlabel("Time [100ms]")
 plt.ylabel("GPU Temp [C]")
+plt.grid(axis="both")
 plt.legend()
 plt.show()
 plt.clf()
@@ -56,6 +88,7 @@ for gpuId in range(len(temperatures_gpu)):
     plt.plot(x_vals, temperatures_mem[gpuId], label="GPU"+str(gpuId))
 plt.xlabel("Time [100ms]")
 plt.ylabel("Mem Temp [C]")
+plt.grid(axis="both")
 plt.legend()
 plt.show()
 plt.clf()
@@ -72,6 +105,7 @@ for ind in range(len(temperatures_gpu[0])):
 plt.plot(x_vals, max_diff_temp)
 plt.xlabel("Time [100ms]")
 plt.ylabel("Max GPU Temp Diff [C]")
+plt.grid(axis="both")
 plt.show()
 
 max_diff_temp = []
@@ -86,6 +120,7 @@ for ind in range(len(temperatures_mem[0])):
 plt.plot(x_vals, max_diff_temp)
 plt.xlabel("Time [100ms]")
 plt.ylabel("Max Mem Temp Diff [C]")
+plt.grid(axis="both")
 plt.show()
 plt.clf()
 
@@ -93,6 +128,7 @@ for gpuId in range(len(temperatures_gpu)):
     plt.plot(x_vals, powers_gpu[gpuId], label="GPU"+str(gpuId))
 plt.xlabel("Time [100ms]")
 plt.ylabel("GPU Power [W]")
+plt.grid(axis="both")
 plt.legend()
 plt.show()
 plt.clf()
@@ -109,6 +145,7 @@ for ind in range(len(powers_gpu[0])):
 plt.plot(x_vals, max_diff_power)
 plt.xlabel("Time [100ms]")
 plt.ylabel("Max GPU Power Diff [W]")
+plt.grid(axis="both")
 plt.show()
 plt.clf()
 
@@ -116,6 +153,7 @@ for gpuId in range(len(frequencies_gpu)):
     plt.plot(x_vals, frequencies_gpu[gpuId], label="GPU"+str(gpuId))
 plt.xlabel("Time [100ms]")
 plt.ylabel("GPU Frequency [MHz]")
+plt.grid(axis="both")
 plt.legend()
 plt.show()
 plt.clf()
